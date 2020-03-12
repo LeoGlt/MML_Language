fb8578bf1de30f62d9d249fd1751c0b9232c2a80
Allan Vitré <vitrallan49@gmail.com>
2020-03-12 12:20:52 +0100

    add jython, display script

--- MML_LANGUAGE/mml-webapp/src/main/resources/public/display_R_Py.js
+++ MML_LANGUAGE/mml-webapp/src/main/resources/public/display_R_Py.js
@@ -8,16 +8,15 @@ function Display_Py(){
     var scriptpy = document.getElementById("Code_Python");
     var script = document.getElementById("script");
     var textpy = document.getElementById("pyscript");
+    var iframe = document.getElementById("iframe_script").contentDocument;
 
     if(this.value == "Display Python script"){
+        
         scriptpy.style.display = 'block';
         this.value = "Undisplay";
         script.style.height = "592px";
 
-        textpy.value = iframe.body.getElementById("script_py");
-
-
-
+        textpy.value = iframe.getElementById("script_py").innerHTML;
     }
     else{
         scriptpy.style.display = 'none';
@@ -25,34 +24,27 @@ function Display_Py(){
         script.style.height = "";
     };
 
-    
-    
-        
-
 }
 
 function Display_R(){
 
     var scriptr = document.getElementById("Code_R");
     var textr = document.getElementById("Rscript");
+    var iframe = document.getElementById("iframe_script").contentDocument;
 
     if(this.value == "Display R script"){
         scriptr.style.display = 'block';
         this.value = "Undisplay";
+        textr.value = iframe.getElementById("script_r").innerHTML;
 
     }
     else{
         scriptr.style.display = 'none';
         this.value = "Display R script";
-        textpy.value = iframe.body.getElementById("script_R");
     };
 
 }
 
 
-button_Py.addEventListener("click",Display_Py)
+button_Py.addEventListener("click",Display_Py);
 button_R.addEventListener("click",Display_R)
-
-
-var iframe = document.getElementById("iframe_script").contentDocument;
-
